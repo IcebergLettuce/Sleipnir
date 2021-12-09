28fcfd72ca49279e5090f9bd0e1325578960dc7a
manuel <manuel.alexander.hirzel@gmail.com>
2021-12-09 15:27:11 +0100

    fixing row of execution

--- components/traefik/install.sh
+++ components/traefik/install.sh
@@ -1,7 +1,8 @@
 
+sudo apt-get update
 sudo apt-get install jq -y
 sudo kubectl apply -f traefik/deployment.yaml
 
 arguments=$(sudo kubectl get deployments traefik -o jsonpath='{.spec.template.spec.containers[0].args}')
-echo $arguments | jq '. |= .+ ["--api.insecure=true"] ' > args
-sudo kubectl patch deployment traefik --type='json' -p="[{\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/args\", \"value\": $(cat args)}]"
+echo $arguments | jq '. |= .+ ["--api.insecure=true"] ' > fixargs
+sudo kubectl patch deployment traefik --type='json' -p="[{\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/args\", \"value\": $(cat fixargs)}]"
