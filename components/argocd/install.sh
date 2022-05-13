c376d673a50cb7bbfad7fae31d012cb04124a1ec
Manuel <manuel_hirzel@hotmail.com>
2022-05-13 18:20:52 +0200

    fixing k8s installation

--- components/argocd/install.sh
+++ components/argocd/install.sh
@@ -1,6 +1,6 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml
 kubectl patch deployment argocd-server -n argocd --patch "$(cat argocd/argocd-patch.yaml)"
-kubectl apply -f argocd/argocd-ingress.
+kubectl apply -f argocd/argocd-ingress.yaml
 apt install apache2-utils -y
 kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" admin | tr -d ':\n')'"}}'
