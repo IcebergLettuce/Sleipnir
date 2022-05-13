f4477d99af59ab98e01fcfea8ac936e540220da8
Manuel <manuel_hirzel@hotmail.com>
2022-05-13 16:40:01 +0200

    fixing k8s installation

--- components/kubernetes/install.sh
+++ components/kubernetes/install.sh
@@ -92,3 +92,15 @@ kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 
 
+# ___________________________
+#
+# DOWNLOAD AND INSTALL HELM
+#
+# ___________________________
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+apt-get update
+apt-get install helm
+
--- components/traefik/install.sh
+++ components/traefik/install.sh
@@ -1,9 +1,19 @@
 
-sudo apt-get update
-sudo apt-get install jq -y
+# ___________________________
+#
+# DOWNLOAD AND INSTALL TRAEFIK INGRESS CONTROLLER
+#
+# ___________________________
+
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm install traefik traefik/traefik
+
+apt-get update
+apt-get install jq -y
 
 arguments=$(sudo kubectl get deployments traefik -o jsonpath='{.spec.template.spec.containers[0].args}')
 echo $arguments | jq '. |= .+ ["--api.insecure=true"] ' > fixargs
-sudo kubectl patch deployment traefik --type='json' -p="[{\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/args\", \"value\": $(cat fixargs)}]"
-sleep 50s
-sudo kubectl apply -f traefik/deployment.yaml
+kubectl patch deployment traefik --type='json' -p="[{\"op\": \"add\", \"path\": \"/spec/template/spec/containers/0/args\", \"value\": $(cat fixargs)}]"
+sleep 120s
+kubectl apply -f traefik/deployment.yaml
